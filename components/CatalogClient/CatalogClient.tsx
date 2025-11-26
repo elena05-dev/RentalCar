5d72469254c39143adce893c859203c6a819ea9f
Elena Polyakova <epolyk1970@gmail.com>
2025-11-26 22:45:02 +0200

    files

--- components/CatalogClient/CatalogClient.tsx
+++ components/CatalogClient/CatalogClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useStore } from "../../store/useStore";
 import VehicleCard from "../VehicleCard/VehicleCard";
 import Filters from "../Filters/Filters";
--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import { useStore } from "../../store/useStore";
 import { Filters as FiltersType } from "../../types/vehicle";
 
